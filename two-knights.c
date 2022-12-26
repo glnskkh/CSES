1de30426b6cc8b00c9dc4dda35613eed7e084332
Georgiy Glinskikh <glnskkh@vk.com>
2022-12-26 22:10:45 +0500

    shoten some code

--- two-knights.c
+++ two-knights.c
@@ -1,30 +1,17 @@
 #include <stdio.h>
 
-/* Get number of positions which are beaten on k*k file by knight */
-long long countBeatenPositions(int k)
-{
-  switch (k)
-  {
-  case 1:
-  case 2:
-    return 0;
-  case 3:
-    return 10 - 2;
-  default:
-    return 18 + 8 * (k - 4) - 2;
-  }
-}
-
 int main(void)
 {
-  long long n, i, t, count = 0;
+  long long n, i, t, count = -8;
 
   /* Get maximum field size: k = 1,2,..,n */
   scanf("%lld", &n);
 
   for (i = 1, t = 1; i <= n; t += 4 * i + 1, ++i)
   {
-    count += (2 * i - 1) * i * i - countBeatenPositions(i) - t;
+    count += (2 * i - 1) * i * i;
+    count -= 8 * i - 16;
+    count -= t;
     printf("%lld\n", count);
   }
 }
