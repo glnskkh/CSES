5dd40111c5d04a3ed70273088cecd5d34ac7e2a2
Georgiy Glinskikh <glnskkh@vk.com>
2022-12-28 14:53:43 +0500

    move two knight into once expression

--- two-knights.c
+++ two-knights.c
@@ -2,16 +2,14 @@
 
 int main(void)
 {
-  long long n, i, t, count = -8;
+  unsigned long long n, i, count = -8;
 
   /* Get maximum field size: k = 1,2,..,n */
   scanf("%lld", &n);
 
-  for (i = 1, t = 1; i <= n; t += 4 * i + 1, ++i)
+  for (i = 1; i <= n; ++i)
   {
-    count += (2 * i - 1) * i * i;
-    count -= 8 * i - 16;
-    count -= t;
+    count += (2 * i - 1) * (i - 1) * i - 8 * (i - 2);
     printf("%lld\n", count);
   }
 }
